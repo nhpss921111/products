8fef1fce5f52c5df0dcf448f6d73714cc1ce7244
Job Lien <nhpss921111@gmail.com>
2018-09-09 11:42:21 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,9 @@
+products = []
+while True:
+    name = input('請輸入商品名稱: ')
+    if name == 'q':
+        break
+    price = input('請輸入商品的價格: ')
+    p = [name, price] # 小清單裡存 name 和 price
+    products.append(p) # 把小清單(p)存進大清單(products)
+print(products)
